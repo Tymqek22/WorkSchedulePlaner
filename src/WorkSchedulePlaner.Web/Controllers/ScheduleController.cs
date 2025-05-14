98acbc0bdd9638bad5d87600c8a80d144738a660
Tymqek22 <133526709+Tymqek22@users.noreply.github.com>
2025-05-14 19:35:22 +0200

    Revert "Employee adding implemented"
    This reverts commit bdb05c53b93d46803f0dc21731b0ca3c5cdca7a7.

--- src/WorkSchedulePlaner.Web/Controllers/ScheduleController.cs
+++ src/WorkSchedulePlaner.Web/Controllers/ScheduleController.cs
@@ -1,9 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System.Globalization;
 using WorkSchedulePlaner.Application.Repository;
-using WorkSchedulePlaner.Application.Schedules.AddEmployee;
-using WorkSchedulePlaner.Domain.Entities;
-using WorkSchedulePlaner.Web.Models;
 using WorkSchedulePlaner.Web.ViewModels;
 
 namespace WorkSchedulePlaner.Web.Controllers
@@ -11,15 +8,10 @@ namespace WorkSchedulePlaner.Web.Controllers
 	public class ScheduleController : Controller
 	{
 		private readonly IWorkScheduleRepository _repository;
-		private readonly IRepository<Employee> _employeeRepository;
-		private readonly AddEmployee _addEmployee;
 
-		public ScheduleController(IWorkScheduleRepository repository, IRepository<Employee> employeeRepository,
-			AddEmployee addEmployee)
+		public ScheduleController(IWorkScheduleRepository repository)
 		{
 			_repository = repository;
-			_employeeRepository = employeeRepository;
-			_addEmployee = addEmployee;
 		}
 
 		public async Task<IActionResult> Details(int id)
@@ -43,45 +35,6 @@ namespace WorkSchedulePlaner.Web.Controllers
 			};
 
 			return View(viewModel);
-		}
-
-		public async Task<IActionResult> Employees(int id)
-		{
-			//temporary
-			var employees = await _employeeRepository.GetAsync(e => e.ScheduleId == id);
-
-			return View(employees.ToList());
-		}
-
-		public IActionResult AddEmployee(int id)
-		{
-			ViewBag.ScheduleId = id;
-
-			return View();
-		}
-
-		[HttpPost]
-		public async Task<IActionResult> AddEmployee(Employee employee)
-		{
-			var request = new AddEmployeeRequest(
-				employee.Name,
-				employee.LastName,
-				employee.Position,
-				employee.ScheduleId);
-
-			var result = await _addEmployee.Handle(request);
-
-			if (result != AddEmployeeResult.Success) {
-
-				var error = new ErrorViewModel
-				{
-					RequestId = "Cannot add employee."
-				};
-
-				return View("Error",error);
-			}
-
-			return RedirectToAction("Details","Schedule",new { id = request.ScheduleId });
 		}
 
 		public IActionResult Create()
--- src/WorkSchedulePlaner.Web/Program.cs
+++ src/WorkSchedulePlaner.Web/Program.cs
@@ -1,6 +1,5 @@
 using Microsoft.EntityFrameworkCore;
 using WorkSchedulePlaner.Application.Repository;
-using WorkSchedulePlaner.Application.Schedules.AddEmployee;
 using WorkSchedulePlaner.Application.ShiftTiles.AssignShift;
 using WorkSchedulePlaner.Application.ShiftTiles.DeleteShift;
 using WorkSchedulePlaner.Application.ShiftTiles.UpdateShift;
@@ -25,7 +24,6 @@ builder.Services.AddScoped<IWorkScheduleRepository,WorkScheduleRepository>();
 builder.Services.AddTransient<AssignShift>();
 builder.Services.AddTransient<DeleteShift>();
 builder.Services.AddTransient<UpdateShift>();
-builder.Services.AddTransient<AddEmployee>();
 
 var app = builder.Build();
 
